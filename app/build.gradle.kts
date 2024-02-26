3b22c2c3835aad2b2ef0cbc1cfc51471f0d76845
Mei Habib <habibmai09@gmail.com>
2024-02-26 16:34:47 +0200

    add app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 
 dependencies {
 
+    //Splash Screen
+    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
