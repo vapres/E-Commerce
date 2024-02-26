3ed75c9729b3f5f6578f42b11033436b72dbb369
Mei Habib <habibmai09@gmail.com>
2024-02-26 22:52:02 +0200

    add splash screen for android 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,8 @@ android {
 dependencies {
 
     //Splash Screen
-    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+//    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
